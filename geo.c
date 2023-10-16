d7abafc6df36b9e5437727b24b544d52a4ee0a83
Aaron Mavrinac <aaron@opreto.com>
2023-10-16 18:23:27 -0400

    Fix formatting.

--- geo.c
+++ geo.c
@@ -10,8 +10,8 @@ double distance(double lat1, double lon1, double lat2, double lon2) {
     double delta_phi = (lat2 - lat1) * deg2rad;
     double delta_lambda = (lon2 - lon1) * deg2rad;
 
-    double a = sin(delta_phi / 2.0l) * sin(delta_phi / 2.0l) + cos(phi1) * cos(phi2) *
-               sin(delta_lambda / 2.0l) * sin(delta_lambda / 2.0l);
+    double a = sin(delta_phi / 2.0l) * sin(delta_phi / 2.0l) +
+               cos(phi1) * cos(phi2) * sin(delta_lambda / 2.0l) * sin(delta_lambda / 2.0l);
     double c = 2.0l * atan2(sqrt(a), sqrt(1.0l - a));
 
     return EARTH_RADIUS * c;
